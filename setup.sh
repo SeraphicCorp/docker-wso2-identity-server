545f9e93cd7adf2a0fe69b08dc56683c394226a6
Ravaka Razafimanantsoa <ravseraphin@gmail.com>
2017-06-12 16:29:51 +0200

    -

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 HOSTNAME=`hostname`
 
-find /opt/wso2/repository/conf/ -type f -name "*.xml" -exec sed -i "s/localhost/$HOSTNAME/g" {} +
+grep -rl 'localhost' /opt/wso2/repository/conf | xargs sed -i "s/localhost/$HOSTNAME/g"
 
 exit 0
